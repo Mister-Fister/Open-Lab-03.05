31d6baa6647c9772863144b7b077e6cfacf24830
Matúš <matusskor@gmail.com>
2020-10-06 19:32:25 +0200

    zmeny

--- Open-Lab-03.05/Comparator.cs
+++ Open-Lab-03.05/Comparator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Open_Lab_03._05
 {
@@ -6,7 +7,11 @@ namespace Open_Lab_03._05
     {
         public bool MatchCaseInsensitive(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.ToLower() == str2.ToLower())
+                 return true;
+                else
+                return false;
+            
         }
     }
 }
